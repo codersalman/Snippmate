74633f11fae459539f0d45171998a394bd5281ed
faiz khan <faizk5515@gmai.com>
2023-09-29 19:50:44 +0530

    Server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/',(req,res)=>{
+    res.send("Success");
+})
+
+app.listen(port,()=>{
+    console.log("Application Running ")
+});
